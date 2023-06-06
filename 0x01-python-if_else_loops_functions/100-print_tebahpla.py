4661f0c1df2afeda8557c32518279e921cad6dc9
dan1bayomi <dan234bayomi@gmail.com>
2023-06-06 13:43:21 -0700

    Attempting reverse alphabet

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def toggle(case):
+    if case is True:
+        return False
+    if case is False:
+        return True
+
+lowercase = True
+for i in range(122, 96, -1):
+    if lowercase is False:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
+    lowercase = toggle(lowercase)

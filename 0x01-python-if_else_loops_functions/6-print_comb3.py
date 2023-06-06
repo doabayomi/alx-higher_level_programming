71d9086fda186f8b23fbf0f541cebc6da5bfe1f9
dan1bayomi <dan234bayomi@gmail.com>
2023-06-06 09:10:12 -0700

    Fixed combinations comma

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,8 @@ for i in range(10):
     for j in range(i, 10):
         if (i == j):
             continue
+        if i == 8 and j == 9:
+            print("{0}{1}".format(i, j), end="")
+            break
         print("{0}{1}".format(i, j), end=", ")
 print()

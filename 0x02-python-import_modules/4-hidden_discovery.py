a75e09d7bf386c9a79d2d1ca3cc1704b23d66220
dan1bayomi <dan234bayomi@gmail.com>
2023-06-08 12:35:58 -0700

    Corrected hidden file task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    print("my_secret_santa")
-    print("print_hidden")
-    print("print_school")
+    names = dir(hidden_4)
+    # listing names in the file
+    valid_names = []
+    for name in names:
+        if name.startswith("__"):
+            continue
+        valid_names.append(name)
+    valid_names = sorted(valid_names)
+
+    # print the valid names
+    for name in valid_names:
+        print(name)

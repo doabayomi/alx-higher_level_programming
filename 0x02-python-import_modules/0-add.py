dbf9349df05302446736918597fac9118e194f81
dan1bayomi <dan234bayomi@gmail.com>
2023-06-08 12:15:19 -0700

    Fixed first task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add_func
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add_func.add(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))

d167bc589e912f59d51a97a5c9ba06b9ccea7d01
dan1bayomi <dan234bayomi@gmail.com>
2023-07-03 10:35:49 -0700

    Checking error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -44,8 +44,7 @@ class Rectangle:
         else:
             if value < 0:
                 raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+            self.__width = value
 
     @property
     def height(self):
@@ -68,5 +67,4 @@ class Rectangle:
         else:
             if value < 0:
                 raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+            self.__height = value

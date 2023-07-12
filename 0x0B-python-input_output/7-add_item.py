740e4110f82bc6687f246aee352ebf2c68e3b946
dan1bayomi <dan234bayomi@gmail.com>
2023-07-12 03:52:56 -0700

    Permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 args = sys.argv[1:]
-
 try:
-    existing_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_list = []
-existing_list.extend(args)
-save_to_json_file(existing_list, "add_item.json")
+    object_data = load_from_json_file("add_item.json")
+except Exception:
+    object_data = []
+object_data.extend(args)
+save_to_json_file(object_data, "add_item.json")

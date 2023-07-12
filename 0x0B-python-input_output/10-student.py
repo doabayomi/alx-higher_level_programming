a7cf3d0f50f170feb44423167847228e1734cc3e
dan1bayomi <dan234bayomi@gmail.com>
2023-07-11 21:56:37 -0700

    Finishing

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Adds method to Student Class to represent in json."""
+
+
+class Student:
+    """Implementation of a student."""
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance.
+
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve Dictionary Representation of object.
+
+        Args:
+            attrs (list, optional): List of attributes to retrieve.
+
+        Returns:
+            dict: Dictionary representaton of object
+        """
+        rep = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+        if attrs is not None:
+            return {attr: rep[attr] for attr in attrs if attr in rep}
+        else:
+            return rep

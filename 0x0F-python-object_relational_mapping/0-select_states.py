cf5363e5f562602822c1af352edbff3e53b317e2
dan1bayomi <dan234bayomi@gmail.com>
2023-09-16 06:53:05 -0700

    Figured it out

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
     u_name = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb._mysql.connect(host="localhost", port=3306, user=u_name,  password=pwd, database=db_name)
-    db.query("""SELECT * FROM states""")
-    r = db.store_result()
-    for i in r.fetch_row(maxrows=0):
-        print(i);
+    db = MySQLdb.connect(host="localhost", port=3306, user=u_name,  password=pwd, database=db_name)
+    c = db.cursor()
+    c.execute("SELECT * FROM states;")
+    states = c.fetchall()
+
+    for state in states:
+        print(state)

9dfcfdb47c529e77203f7d2dbdc54816e5b7b7db
dan1bayomi <dan234bayomi@gmail.com>
2023-09-16 04:53:03 -0700

    Trying to check validity

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,12 @@
 import sys
 import MySQLdb
 
-u_name = sys.argv[1]
-pwd = sys.argv[2]
-db_name = sys.argv[3]
-db = MySQLdb._mysql.connect(host="localhost", port=3306, user=u_name,  password=pwd, database=db_name)
-db.query("""SELECT * FROM states""")
-r = db.store_result()
-for i in r.fetch_row(maxrows=0):
-    print(i);
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb._mysql.connect(host="localhost", port=3306, user=u_name,  password=pwd, database=db_name)
+    db.query("""SELECT * FROM states""")
+    r = db.store_result()
+    for i in r.fetch_row(maxrows=0):
+        print(i);

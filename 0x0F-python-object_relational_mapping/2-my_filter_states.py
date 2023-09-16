362bcd85a369cd515dfaa748373e4cf936677504
dan1bayomi <dan234bayomi@gmail.com>
2023-09-16 07:17:15 -0700

    Fixed 3rd task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with N"""
+"""Filter states based on matching arguments"""
 import sys
 import MySQLdb
 
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     u_name = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
+    state = sys.argv[4]
     db = MySQLdb.connect(host="localhost", port=3306, user=u_name,
                          password=pwd, database=db_name)
     c = db.cursor()
-    c.execute("SELECT * FROM states;")
+    c.execute("SELECT * FROM states WHERE name='{}';".format(state))
     states = c.fetchall()
 
     for state in states:
-        if state[1].startswith("N"):
-            print(state)
+        print(state)

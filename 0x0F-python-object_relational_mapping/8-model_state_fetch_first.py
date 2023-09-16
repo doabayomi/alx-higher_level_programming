1529e391d5edb21e2c09cb3dde5669504713fa87
dan1bayomi <dan234bayomi@gmail.com>
2023-09-16 08:01:07 -0700

    Finished 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
+    u_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(u_name, password, db_name),
                            pool_pre_ping=True)

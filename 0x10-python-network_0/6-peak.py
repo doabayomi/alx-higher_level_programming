d389718bb23b85a0024012d1d7777e9d96e1039d
Daniel Abayomi <dan234bayomi@gmail.com>
2023-09-28 16:27:01 -0700

    Fixed bug in 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,7 +16,7 @@ def find_peak(list_of_integers):
     
     peak = list_of_integers[0]
     for n in list_of_integers:
-        if n > peak;
+        if n > peak:
             peak = n
     
     return peak

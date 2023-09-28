b9d52354e144efaf775c5b4507b60549f702fca4
dan1bayomi <dan234bayomi@gmail.com>
2023-09-28 15:07:15 -0700

    Finished 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Finding the peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Find the peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers
+
+    Returns:
+        int: The peak/highest value in the list
+    """
+    if not list_of_integers:
+        return None
+    
+    peak = list_of_integers[0]
+    for n in list_of_integers:
+        if n > peak;
+            peak = n
+    
+    return peak

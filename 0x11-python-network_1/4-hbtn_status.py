a424760dfb8c7de711f2ac63e0d0b4bcf778193a
doabayomi <66203418+doabayomi@users.noreply.github.com>
2023-12-04 23:53:52 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status using requests"""
-import requests
-
-
-if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
--- 0x11-python-network_1/README.md
+++
@@ -1,5 +0,0 @@
-# Description
-This is the continuation of the Network on Pythonf or the ALX SE program
-
-# Notes
-None for now
